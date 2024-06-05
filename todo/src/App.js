14d3b113aec2cb14b5999cc8d9373b420e6520c0
Nathan  Aguilar <nathanaguilar@Nathans-MBP.lan>
2024-06-05 16:12:23 -0700

    Adding User Auth

---
+++ todo/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Amplify } from 'aws-amplify'
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+const App = () => {
+  return (
+    <Authenticator>
+      {({ user}) => (
+        <main>
+          <h1>Welcome {user.username}</h1>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
+
+export default App;
